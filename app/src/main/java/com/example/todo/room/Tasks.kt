c6aa218016c74e2f7464c41e6dcfa18b21ca0855
Akbar Jaborov <bari4815@gmail.com>
2022-08-31 22:19:44 +0500

    First Commit

---
+++ app/src/main/java/com/example/todo/room/Tasks.kt
@@ -0,0 +1,21 @@
+package com.example.todo.room
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tasks_table")
+
+data class Tasks(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "taskId")
+    val taskId: Int,
+    @ColumnInfo(name = "taskText")
+    val taskText: String,
+    @ColumnInfo(name = "taskLat")
+    val taskLat: Double,
+    @ColumnInfo(name = "taskLon")
+    val taskLon: Double,
+    @ColumnInfo(name = "status")
+    val status: Boolean
+)
